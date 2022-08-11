09578fe8bf3aaa218bd2c9eae702ed2d5ad69a8f
Pauline Thomas <pauline.farina90@gmail.com>
2022-08-11 21:10:47 +0200

    solved lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+/* GET movie page */
+router.get("/movies", (req, res, next) => {
+  const importedMovieSchema = require("../models/Movies.model.js");
+  importedMovieSchema.find({}).then((movies) => {
+    console.log("found them", movies);
+    res.render("movies", { movies });
+  });
+});
+
+router.get("/movies/:moviesId", (req, res, next) => {
+  const { moviesId } = req.params;
+  const importedMovieSchema = require("../models/Movies.model.js");
+  importedMovieSchema.findById(moviesId).then((movie) => {
+    console.log("found the movieee with the ID");
+    res.render("movie", movie);
+  });
+});
 
 module.exports = router;
